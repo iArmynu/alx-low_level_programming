5032bdb54930c7063e9640342e0c434de8bdf1fc
iArmynu <armynu2012@gmail.com>
2022-10-18 18:27:05 -0700

    function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@ void jack_bauer(void)
 {
 	int hour, minute;
 
-	for (hour = 0; hour <= 23; hour++)
+	for (hour = 0; hour <= 24; hour++)
 	{
-		for (minute = 0; minute <= 59; minute++)
+		for (minute = 0; minute <= 60; minute++)
 		{
 			_putchar((hour / 10) + '0');
 			_putchar((hour % 10) + '0');
@@ -18,7 +18,6 @@ void jack_bauer(void)
 			_putchar((minute / 10) + '0');
 			_putchar((minute % 10) + '0');
 			_putchar('\n');
-			return (0);
 		}
 	}
 }
